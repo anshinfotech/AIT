ee64e1f2a746cafe107c34869a970933a27bed51
Ansh Infotech <52543676+anshinfotech@users.noreply.github.com>
2024-03-29 13:34:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,10 @@ const fs = require("fs");
 const DB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://ait:ait@cluster0.elwntta.mongodb.net/data"
+      "mongodb+srv://ait:ait@cluster0.elwntta.mongodb.net/data",{
+         serverSelectionTimeoutMS: 30000, // 30 seconds
+  socketTimeoutMS: 45000, // 45 seconds
+      }
     );
     console.log("databse connected");
   } catch (error) {
